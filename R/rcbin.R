504631817417a982df34f84d1902a7c1735d8f5d
akhtarh <mhossain@email.sc.edu>
2017-09-19 11:23:11 -0400

    Minor editorial corrections for version 1.1

--- R/rcbin.R
+++ R/rcbin.R
@@ -1,11 +1,11 @@
 #' Generates correlated binary cluster data
 #'
 #' Generates correrlated binary cluster data given value of Intracluster Correlation, proportion of event and it's variation, number of clusters, cluster size and variation in cluster size
-#' @param prop A numeric value between 0 to 1 denoting assumed proportion of event in interest, default value is 0.5
-#' @param prvar A numeric value between 0 to 1 denoting percent of variation in assumed proportion of event (\code{prvar}), default value is 0
+#' @param prop A numeric value between 0 to 1 denoting assumed proportion of event in interest. Default value is 0.5
+#' @param prvar A numeric value between 0 to 1 denoting percent of variation in assumed proportion of event (\code{prop}). Default value is 0
 #' @param noc A numeric value telling the number of clusters to be generated
 #' @param csize A numeric value denoting desired cluster size
-#' @param csvar A numeric value between 0 to 1 denoting percent of variation in cluster sizes (\code{csize}), default value is 0
+#' @param csvar A numeric value between 0 to 1 denoting percent of variation in cluster sizes (\code{csize}). Default value is 0
 #' @param rho A numeric value between 0 to 1 denoting desired level of Intracluster Correlation
 #' @return A dataframe with two columns presenting cluster id (\code{cid}) and a binary response (\code{y}) variables
 #'
@@ -33,7 +33,7 @@ rcbin <- function(prop = .5, prvar = 0, noc, csize, csvar = 0, rho){
     while(csizen < min_csize | csizen > max_csize){
       csizen <- abs(round(csize + (csize*csvar)*rnorm(1)))
     }
-    # Selecting individual cluster properties
+    # Selecting individual cluster event proportion
     min_prop <- prop - prop*prvar
     max_prop <- prop + prop*prvar
     propn <- abs(prop + (prop*prvar)*rnorm(1))
